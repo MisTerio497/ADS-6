8637d23804de9a7b6a572b7e4a22ff26c5dc99f3
MisTerio497 <ipkovalenko2006@gmail.com>
2025-04-23 13:10:14 +0300

    1

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,107 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+#include <iostream>
+#include <stdexcept>
+
+template <typename T> struct Node {
+  T data;
+  Node *next;
+  Node(const T &data) : data(data), next(nullptr) {}
+};
+
+template <typename T> class TPQueue {
+  Node<T> *head;
+  Node<T> *tail;
+  int count;
+
+public:
+  TPQueue() : head(nullptr), tail(nullptr), count(0) {}
+
+  ~TPQueue() {
+    while (head) {
+      Node<T> *temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  void push(const T &item) {
+    Node<T> *newNode = new Node<T>(item);
+
+    if (!head) {
+      head = tail = newNode;
+      count++;
+      return;
+    }
+
+    if (item.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      count++;
+      return;
+    }
+
+    Node<T> *current = head;
+    while (current->next && item.prior <= current->next->data.prior) {
+      current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+
+    if (!newNode->next) {
+      tail = newNode;
+    }
+    count++;
+  }
+
+  void peek() const {
+    if (!head) {
+      throw std::underflow_error("Queue is empty");
+    }
+    std::cout << head->data.ch << "-" << head->data.prior << '\n';
+  }
+
+  void pop(char ch) {
+    if (!head) {
+      throw std::underflow_error("Queue is empty");
+    }
+
+    if (head->data.ch == ch) {
+      Node<T> *temp = head;
+      head = head->next;
+      if (!head) {
+        tail = nullptr;
+      }
+      delete temp;
+      count--;
+      return;
+    }
+
+    Node<T> *current = head;
+    while (current->next && current->next->data.ch != ch) {
+      current = current->next;
+    }
+
+    if (!current->next) {
+      throw std::runtime_error("Element not found");
+    }
+
+    Node<T> *temp = current->next;
+    current->next = temp->next;
+
+    if (!current->next) {
+      tail = current;
+    }
+
+    delete temp;
+    count--;
+  }
+
+  bool isEmpty() const { return head == nullptr; }
+
+  int size() const { return count; }
 };
 
 struct SYM {
@@ -12,4 +110,4 @@ struct SYM {
   int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUE_H_
